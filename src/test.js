aafeee1e7dc77d9c6319807e161da127ca9e4c4a
ParasPaul11 <reachtoparas@gmail.com>
2024-08-11 20:52:17 +0530

    commit changes

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Trend, Counter, Rate, Gauge } from 'k6/metrics';
 
-//MyCodeParas
+//MyCodeParasPaul
 // Custom metrics
 const waitingTime = new Trend('waiting_time'); // Tracks waiting time
 const myCounter = new Counter('successful_requests'); // Counts successful requests
